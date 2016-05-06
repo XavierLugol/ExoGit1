13e0d8a634ba17451c0bd4b04c06869a92870389
xavier <xavier.lugol@laposte.net>
2016-05-06 13:45:12 +0200

    New version

--- src/ExoGit1.java
+++ src/ExoGit1.java
@@ -1,23 +1,55 @@
 import java.util.Scanner;
 
 public class ExoGit1 {
-	private int bigint1;
-	private int bigint2;
-	private int bigint3;
+	private static int bigint1;
+	private static int bigint2;
+	private static int bigint3;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 0;
 		Scanner sc = new Scanner(System.in);
+		String myString;
 
-		while (true) {
-			String myString = new String(sc.nextLine());
-			if (myString.equals("-1")) {
-				break;
+		do {
+			myString = new String(sc.nextLine());
+			i = Integer.parseInt(myString);
+			manageRange(i);
+			if (!(i == -1)) {
+				System.out.println("3 plus grands : " + bigint1 + ", " + bigint2 + ", " + bigint3);
 			}
+		} while (!myString.equals("-1"));
+		System.out.println("Fini");
+	}
 
+	private static int mostSmall() {
+		if (bigint1 <= bigint2 && bigint1 <= bigint3) {
+			return 1;
+		}
+		if (bigint2 <= bigint1 && bigint2 <= bigint3) {
+			return 2;
+		}
+		return 3;
+	}
+
+	private static void manageRange(int i) {
+		int small = mostSmall();
+		switch (small) {
+		case 1:
+			if (i > bigint1) {
+				bigint1 = i;
+			}
+			break;
+		case 2:
+			if (i > bigint2) {
+				bigint2 = i;
+			}
+			break;
+		case 3:
+			if (i > bigint3) {
+				bigint3 = i;
+			}
 		}
 
 	}
-
 }
