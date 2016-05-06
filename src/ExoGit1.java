8122bcc0395f1080bc98b170180fb55111fef1b1
xavier <xavier.lugol@laposte.net>
2016-05-06 11:21:43 +0200

    Ajout des attributs

--- src/ExoGit1.java
+++ src/ExoGit1.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class ExoGit1 {
+	private int bigint1;
+	private int bigint2;
+	private int bigint3;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 0;
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			String myString = new String(sc.nextLine());
+			if (myString.equals("-1")) {
+				break;
+			}
+
+		}
 
 	}
 
